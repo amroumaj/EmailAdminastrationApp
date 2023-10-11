52d607ec19df02ca70197e2ff6400f612624cefb
amroumaj <amrou.majdoub@etudiant-enit.utm.tn>
2023-10-12 00:36:26 +0100

    completed the program barring the setters and getters

--- Email.java
+++ Email.java
@@ -1,10 +1,15 @@
+import java.util.Scanner;
+
 public class Email {
    private String firstName;
    private String lastName;
    private String password;
    private String department;
    private String alternateEmail;
-   private int mailBoxCapacity;
+   private String email;
+   private String companySuffix = "company.com";
+   private int mailBoxCapacity = 500;
+   private int passwordLength = 10;
 
    //Constructor
 
@@ -13,7 +18,28 @@ public class Email {
      this.lastName = lastName;
      System.out.println("Email created: "+this.firstName+" "+this.lastName);
 
+     //Call a method that asks for the department
      this.department = setDepartment();
+     System.out.println("Department: "+ this.department);
+
+     //Call a method that returns a random password
+     this.password = randomPassword(passwordLength);
+     System.out.println("Your password is: " + this.password);
+
+     //Generate the email
+     email = firstName.toLowerCase()+"."+lastName.toLowerCase()+"@"+department+"."+companySuffix;
+     System.out.println("Your email is: "+ email);
+   }
+  
+   //Generate a random password
+   private String randomPassword(int length){
+     String passwordSet = "ABCEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%";
+     char[] password = new char[length];
+     for (int i=0; i<length; i++){
+       int rand = (int) (Math.random()*passwordSet.length());
+       password[i] = passwordSet.charAt(rand);
+     }
+     return new String(password);
    }
 
    //Ask for the department
@@ -25,16 +51,12 @@ public class Email {
      switch (departmentChoice){
        case 1:
          return "sales";
-         break;
        case 2:
          return "dev";
-         break;
        case 3:
          return "acct";
-         break;
        case 4:
          return "HR";
-         break;
        default:
           return "";
      }
